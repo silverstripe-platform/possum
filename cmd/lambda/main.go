b6340d0ec4cf197f03bea84d81f4b10689fce205
Ben Percy <ben.percy@silverstripe.com>
2025-05-26 14:32:58 +1200

    updated silverstripeltd repo name to silverstripe-platform

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/slack-go/slack"
-	"github.com/silverstripeltd/possum"
+	"github.com/silverstripe-platform/possum"
 )
 
 func main() {
